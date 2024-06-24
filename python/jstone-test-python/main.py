fa82ecde123d79b7f442d9c6ec0a90c507fb2b48
zkzz7@naver.com <zkzz7@naver.com>
2024-06-24 17:57:15 +0900

    2024-06-24 jstone-test

--- python/jstone-test-python/main.py
+++ python/jstone-test-python/main.py
@@ -2,65 +2,17 @@ import boto3
 from botocore.config import Config
 
 service_name = 's3'
-access_key = 'a1a59c6954024fe9ab45b7faa52d1e29'
-secret_key = 'edd0189cab4b47a09e92149da237ecd0'
+access_key = '729184ca0f1e4e13852466b94e2cb2c0'
+secret_key = 'b8e3e3417c6b47938d2e45c8bb9b80eb'
 endpoint_url = 'http://storage.gscdn.com'
 region_name = 'us-east-1'
-
 my_config = Config(
-        signature_version='s3v4'
-)
+        signature_version='s3v4')
 
 if __name__ == "__main__":
         s3 = boto3.client(service_name, endpoint_url=endpoint_url, region_name=region_name, config=my_config, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-        bucket_name = 'test'
 
-        # list all in the bucket
-        max_keys = 300
-        response = s3.list_objects(Bucket=bucket_name, MaxKeys=max_keys)
+response = s3.list_buckets()
 
-        print('list all in the bucket')
-
-        while True:
-                print('IsTruncated=%r' % response.get('IsTruncated'))
-                print('Marker=%s' % response.get('Marker'))
-                print('NextMarker=%s' % response.get('NextMarker'))
-
-                print('Object List')
-                for content in response.get('Contents'):
-                        print(' Name=%s, Size=%d, Owner=%s' % (content.get('Key'), content.get('Size'), content.get('Owner').get('ID')))
-
-                if response.get('IsTruncated'):
-                        response = s3.list_objects(Bucket=bucket_name, MaxKeys=max_keys,
-                        Marker=response.get('NextMarker'))
-                else:
-                        break
-
-        # top level folders and files in the bucket
-        delimiter = '/'
-        max_keys = 300
-
-        response = s3.list_objects(Bucket=bucket_name, Delimiter=delimiter, MaxKeys=max_keys)
-
-        print('top level folders and files in the bucket')
-
-        while True:
-                print('IsTruncated=%r' % response.get('IsTruncated'))
-                print('Marker=%s' % response.get('Marker'))
-                print('NextMarker=%s' % response.get('NextMarker'))
-
-                if response.get('CommonPrefixes') is not None:
-                        print('Folder List')
-                        for folder in response.get('CommonPrefixes'):
-                                print(' Name=%s' % folder.get('Prefix'))
-
-                if response.get('Contents') is not None:
-                        print('File List')
-                        for content in response.get('Contents'):
-                                print(' Name=%s, Size=%d, Owner=%s' % (content.get('Key'), content.get('Size'), content.get('Owner').get('ID')))
-
-                if response.get('IsTruncated'):
-                        response = s3.list_objects(Bucket=bucket_name, Delimiter=delimiter, MaxKeys=max_keys, Marker=response.get('NextMarker'))
-
-                else:
-                        break
+for bucket in response.get('Buckets', []):
+        print(bucket.get('Name'))
